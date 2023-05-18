795b189279b1c01f6ac7e679512bb1969553b1f4
Bruno Pantaleão <bruno.ing879@gmail.com>
2023-05-18 14:20:42 +0200

    Initial Commit

---
+++ ShortcutsServer/ShortcutsServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutsServerApp.swift
+//  ShortcutsServer
+//
+//  Created by Bruno Pantaleão on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutsServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
